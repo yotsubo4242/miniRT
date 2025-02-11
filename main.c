1b5e1cfe4565ad29a499bb776d3672be8d761141
yotsubo <513047.otsubo@gmail.com>
2025-02-11 16:22:04 +0900

    球とレイの交差検証作成.

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: yuotsubo <yuotsubo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 13:41:09 by yuotsubo          #+#    #+#             */
-/*   Updated: 2025/02/06 18:11:42 by yuotsubo         ###   ########.fr       */
+/*   Updated: 2025/02/11 15:38:38 by yuotsubo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #define HEIGHT 1000
 #define OBSRV_X 0
 #define OBSRV_Y 0
-#define OBSRV_Z 2500
+#define OBSRV_Z -2500
 #define SPHERE_X 0
 #define SPHERE_Y 0
 #define SPHERE_Z 2500
@@ -58,7 +58,7 @@ int main(void)
 		{
 			// スクリーンの中心が原点となるように座標を変換.
 			screen.x = x - WIDTH / 2;
-			screen.y = y - HEIGHT / 2;
+			screen.y = HEIGHT / 2 - y;
 			screen.z = 0;
 
 			// 視点からスクリーン上の点へのベクトルを計算.
@@ -68,40 +68,15 @@ int main(void)
 
 			double	a, b, c;
 			a = dot(ray, ray);
-			b = dot(obs, ray) - dot(sphere, ray);
-			c = dot(obs, obs) - 2 * dot(sphere, obs) + dot(sphere, sphere)  - pow(SPHERE_RADIUS, 2);
+			b = 2 * (dot(obs, ray) - dot(sphere, ray));
+			c = dot(obs, obs) - 2 * dot(sphere, obs) + dot(sphere, sphere) - pow(SPHERE_RADIUS, 2);
 
 			double D = pow(b, 2) - 4 * a * c;
 			if (D < 0)
-			{
-				my_mlx_pixel_put(&data, x, y, 0x00FFFF);
-				continue;
-			}
-			double t1 = (-b + sqrt(D)) / 2 * a;
-			double t2 = (-b - sqrt(D)) / 2 * a;
-			printf("x: %d, y: %d, t1: %f, t2: %f\n", x, y, t1, t2); fflush(stdout);
-			if (t1 < 0 && t2 < 0)
-			{
 				my_mlx_pixel_put(&data, x, y, 0x000000);
-				continue;
-			}
-			else if (t1 > 0)
-			{
-				my_mlx_pixel_put(&data, x, y, 0xFFFF00);
-				continue;
-			}
-			else if (t2 > 0)
-			{
-				my_mlx_pixel_put(&data, x, y, 0x00FF00);
-				continue;
-			}
+			else
+				my_mlx_pixel_put(&data, x, y, 0xFFFFFF);
 		}
-	}
-
-	for (int i = 0; i < 100; i++)
-	{
-		for (int j = 0; j < 100; j++)
-			my_mlx_pixel_put(&data, i, j, 0xFF0000);
 	}
 	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.img, 0, 0);
 	mlx_key_hook(data.win_ptr, my_exit, NULL);
