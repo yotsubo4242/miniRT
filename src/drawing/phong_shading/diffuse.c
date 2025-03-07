043c7091b5116773c0576cb490df3d29abc08d47
yotsubo <513047.otsubo@gmail.com>
2025-03-07 15:13:48 +0900

    [#61] done: adjust light color

--- src/drawing/phong_shading/diffuse.c
+++ src/drawing/phong_shading/diffuse.c
@@ -6,7 +6,7 @@
 /*   By: yuotsubo <yuotsubo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 12:59:21 by yuotsubo          #+#    #+#             */
-/*   Updated: 2025/03/07 14:46:33 by yuotsubo         ###   ########.fr       */
+/*   Updated: 2025/03/07 15:02:50 by yuotsubo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,33 @@ void	make_intersection(t_vec3 *intersection, double t, t_scene scene)
 	intersection->z = scene.camera.position.z + t * scene.ray.z;
 }
 
-double	diffuse(t_scene scene, double brightness)
+static t_radiance	caluc_diffuse_radiance(t_scene scene, \
+										double nl, double brightness)
 {
-	t_vec3	l;
-	double	nl;
-	double	r_d;
+	t_radiance	r_d;
+
+	r_d.r_radiance = nl * KD * brightness * (scene.light.color.r / 255);
+	if (r_d.r_radiance < 0)
+		r_d.r_radiance = 0;
+	r_d.g_radiance = nl * KD * brightness * (scene.light.color.g / 255);
+	if (r_d.g_radiance < 0)
+		r_d.g_radiance = 0;
+	r_d.b_radiance = nl * KD * brightness * (scene.light.color.b / 255);
+	if (r_d.b_radiance < 0)
+		r_d.b_radiance = 0;
+	return (r_d);
+}
+
+t_radiance	diffuse(t_scene scene, double brightness)
+{
+	t_vec3		l;
+	double		nl;
+	t_radiance	r_d;
 
 	l = vec_normalize(vec_minus(scene.light.position, scene.inter));
 	nl = vec_dot(scene.n, l);
 	if (nl < 0)
 		nl = vec_dot(vec_rev(scene.n), l);
-	r_d = nl * KD * brightness;
-	if (r_d < 0)
-		r_d = 0;
+	r_d = caluc_diffuse_radiance(scene, nl, brightness);
 	return (r_d);
 }
